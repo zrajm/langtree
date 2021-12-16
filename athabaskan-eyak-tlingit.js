60c2333aec983c72d6a60acaa81a369ab87e8153
zrajm <zrajm@klingonska.org>
2021-12-16 05:06:52 +0100

    Added data for Inuit and Athabaskan language trees

---
+++ athabaskan-eyak-tlingit.js
@@ -0,0 +1,193 @@
+//-*- js-indent-level: 2; css-indent-offset: 2 -*-
+// Data from: https://glottolog.org/glottolog/family
+let treeData = { "name": "Athabaskan-Eyak-Tlingit", "glottolog": "atha1245", "type": "family", "children": [
+  { "name": "Athabaskan-Eyak", "glottolog": "atha1246", "type": "family", "children": [
+    { "name": "Athabaskan", "glottolog": "atha1247", "type": "family", "children": [
+      { "name": "Apachean", "glottolog": "apac1239", "type": "family", "children": [
+        { "name": "Kiowa Apache", "glottolog": "kiow1264", "type": "language", "cas": false },
+        { "name": "Southwestern Apachean", "glottolog": "sout3151", "type": "family", "children": [
+          { "name": "Eastern Southwestern Apachean", "glottolog": "east2723", "type": "family", "children": [
+            { "name": "Jicarilla Apache", "glottolog": "jica1244", "type": "language", "cas": false },
+            { "name": "Lipan Apache", "glottolog": "lipa1241", "type": "language", "cas": false },
+          ]},
+          { "name": "Western Southwestern Apachean", "glottolog": "west2810", "type": "family", "children": [
+            { "name": "Mescalero-Chiricahua Apache", "glottolog": "mesc1238", "type": "language", "cas": false },
+            { "name": "Navajo", "glottolog": "nava1243", "type": "language", "cas": false },
+            { "name": "Western Apache", "glottolog": "west2615", "type": "language", "cas": false },
+          ]},
+        ]},
+      ]},
+      { "name": "Central Alaska-Yukon Athabaskan", "glottolog": "cent2371", "type": "family", "children": [
+        { "name": "Gwichin-Han", "glottolog": "gwic1236", "type": "family", "children": [
+          { "name": "Gwich’in", "glottolog": "gwic1235", "type": "language", "cas": false, "children": [
+            { "name": "Alaskan Kutchin", "glottolog": "alas1257", "type": "dialect" },
+            { "name": "Canadian Kutchin", "glottolog": "cana1271", "type": "dialect" },
+          ]},
+          { "name": "Hän", "glottolog": "hann1241", "type": "language", "cas": false, "children": [
+            { "name": "Dawson Han", "glottolog": "daws1234", "type": "dialect" },
+            { "name": "Eagle Han", "glottolog": "eagl1234", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Koyukonic", "glottolog": "koyu1238", "type": "family", "children": [
+          { "name": "Degexit’an", "glottolog": "dege1248", "type": "language", "cas": false, "children": [
+            { "name": "Lower Yukon Ingalik", "glottolog": "lowe1452", "type": "dialect" },
+            { "name": "Middle Kuskokwim Ingalik", "glottolog": "midd1368", "type": "dialect" },
+          ]},
+          { "name": "Holikachuk", "glottolog": "holi1241", "type": "language", "cas": false },
+          { "name": "Koyukon", "glottolog": "koyu1237", "type": "language", "cas": false, "children": [
+            { "name": "Central Koyukon", "glottolog": "cent2374", "type": "dialect" },
+            { "name": "Lower Koyukon", "glottolog": "lowe1453", "type": "dialect" },
+            { "name": "Upper Koyukon", "glottolog": "uppe1474", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Tanana-Tutchone", "glottolog": "tana1299", "type": "family", "children": [
+          { "name": "Tananaic", "glottolog": "tana1298", "type": "family", "children": [
+            { "name": "Lower Tanana", "glottolog": "lowe1425", "type": "language", "cas": false, "children": [
+              { "name": "Chena (Lower Tanana)", "glottolog": "chen1270", "type": "dialect" },
+              { "name": "Minto-Tolovana-Toklat-Nenana-Wood River", "glottolog": "mint1240", "type": "dialect" },
+              { "name": "Salcha-Goodpastor", "glottolog": "salc1239", "type": "dialect" },
+            ]},
+            { "name": "Upper Kuskokwim", "glottolog": "uppe1438", "type": "language", "cas": false },
+            { "name": "Upper Tananaic", "glottolog": "uppe1472", "type": "family", "children": [
+              { "name": "Tanacross", "glottolog": "tana1290", "type": "language", "cas": false },
+              { "name": "Upper Tanana", "glottolog": "uppe1437", "type": "language", "cas": false, "children": [
+                { "name": "Northway", "glottolog": "nort3356", "type": "dialect" },
+                { "name": "Tetlin", "glottolog": "tetl1234", "type": "dialect" },
+              ]},
+            ]},
+          ]},
+          { "name": "Tutchone", "glottolog": "tutc1236", "type": "family", "children": [
+            { "name": "Northern Tutchone", "glottolog": "nort2941", "type": "language", "cas": false },
+            { "name": "Southern Tutchone", "glottolog": "sout2957", "type": "language", "cas": false },
+          ]},
+        ]},
+      ]},
+      { "name": "Central British Columbia Athabaskan", "glottolog": "cent2370", "type": "family", "children": [
+        { "name": "Carrieric", "glottolog": "carr1250", "type": "family", "children": [
+          { "name": "Dakelh", "glottolog": "carr1248", "type": "family", "children": [
+            { "name": "Central Carrier", "glottolog": "carr1249", "type": "language", "cas": "ᑕᗸᒡ, Dakeł", "year": 1885, "children": [
+              { "name": "Nak’azdli", "glottolog": "naka1267", "type": "dialect" },
+              { "name": "Tl’azt’en", "glottolog": "tlaz1236", "type": "dialect" },
+              { "name": "Yekooche", "glottolog": "yeko1241", "type": "dialect" },
+            ]},
+            { "name": "Southern Carrier", "glottolog": "sout2958", "type": "language", "cas": false, "children": [
+              { "name": "Blackwater Carrier", "glottolog": "blac1272", "type": "dialect", "children": [
+                { "name": "Kluskus", "glottolog": "klus1234", "type": "dialect" },
+                { "name": "Nazko", "glottolog": "nazk1234", "type": "dialect" },
+                { "name": "Red Bluff", "glottolog": "redb1234", "type": "dialect" },
+                { "name": "Ulgatcho", "glottolog": "ulga1234", "type": "dialect" },
+              ]},
+              { "name": "Nechako-Fraser", "glottolog": "nech1234", "type": "dialect", "children": [
+                { "name": "Cheslatta", "glottolog": "ches1239", "type": "dialect" },
+                { "name": "Lheidli", "glottolog": "lhei1234", "type": "dialect" },
+                { "name": "Nadleh", "glottolog": "nadl1234", "type": "dialect" },
+                { "name": "Stellakoh", "glottolog": "stel1239", "type": "dialect" },
+                { "name": "Stoney Creek", "glottolog": "ston1243", "type": "dialect" },
+              ]},
+            ]},
+          ]},
+          { "name": "Witsuwit’en-Babine", "glottolog": "babi1235", "type": "language", "cas": false, "children": [
+            { "name": "Babine-Takla", "glottolog": "babi1236", "type": "dialect" },
+            { "name": "Witsuwit’en", "glottolog": "wits1234", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Chilcotin-Nicola", "glottolog": "chil1280", "type": "language", "cas": false, "children": [
+          { "name": "Nicola Valley Athabaskan", "glottolog": "nico1265", "type": "dialect" },
+          { "name": "Tsilhqut’in", "glottolog": "tsil1234", "type": "dialect" },
+        ]},
+      ]},
+      { "name": "Northwestern Canada Athabaskan", "glottolog": "nort3354", "type": "family", "children": [
+        { "name": "Chipewyan", "glottolog": "chip1261", "type": "language", "cas": "ᑌᓀᓱᐠᑦᕄᓀ, Dënesųłıné", "children": [
+          { "name": "Eastern Chipewyan", "glottolog": "east2857", "type": "dialect" },
+          { "name": "Old Yellowknife Chipewyan", "glottolog": "oldy1234", "type": "dialect" },
+          { "name": "Western Chipewyan", "glottolog": "west2954", "type": "dialect" },
+        ]},
+        { "name": "Cordillera Athabaskan", "glottolog": "cord1235", "type": "family", "children": [
+          { "name": "Beaver-Sekani", "glottolog": "beav1237", "type": "family", "children": [
+            { "name": "Beaver", "glottolog": "beav1236", "type": "language", "cas": "ᑕᓀᖚ ᖚᗀᐥ, Dane-zaa Ẕáágéʔ" },
+            { "name": "Sekani", "glottolog": "seka1250", "type": "language", "cas": false },
+          ]},
+          { "name": "Nahanni", "glottolog": "tahl1238", "type": "family", "children": [
+            { "name": "Kaska", "glottolog": "kask1239", "type": "language", "cas": false },
+            { "name": "Tagish", "glottolog": "tagi1240", "type": "language", "cas": false },
+            { "name": "Tahltan", "glottolog": "tahl1239", "type": "language", "cas": false },
+          ]},
+        ]},
+        { "name": "Slaveyic", "glottolog": "hare1243", "type": "family", "children": [
+          { "name": "Dogrib", "glottolog": "dogr1252", "type": "language", "cas": false },
+          { "name": "Slave", "glottolog": "slav1253", "type": "family", "children": [
+            { "name": "North Slavey", "glottolog": "nort2942", "type": "language", "cas": "ᓴᑋᕲᖌᑎᑊᐠᓀᐠ ᔭᕱ, Sahtúot’ı̨nę Yatı̨́", "year": 1911, "children": [
+              { "name": "Bearlake Slavey", "glottolog": "bear1241", "type": "dialect", "cas": "ᓴᑋᕲᒼᑯᑎᑊᓀ, Sahtúgot’ine" },
+              { "name": "Hare", "glottolog": "hare1244", "type": "dialect", "cas": "ᑲᑊᗱᑯᑎᑊᓀ, K’áshogot’ine" },
+              { "name": "Mountain Slavey", "glottolog": "moun1257", "type": "dialect", "cas": "ᗰᑋᑯᑎᑊᓀ, Shıhgot’ıne" },
+            ]},
+            { "name": "South Slavey", "glottolog": "sout2959", "type": "language", "cas": "ᑌᓀ ᔪᕠᐁ, Dene Zhatié" },
+          ]},
+        ]},
+      ]},
+      { "name": "Pacific Coast Athabaskan", "glottolog": "paci1277", "type": "family", "children": [
+        { "name": "California Athabaskan", "glottolog": "cali1245", "type": "family", "children": [
+          { "name": "Eel River Athabaskan", "glottolog": "wail1244", "type": "language", "cas": false, "children": [
+            { "name": "Lassik", "glottolog": "lass1240", "type": "dialect" },
+            { "name": "Nongatl", "glottolog": "nong1249", "type": "dialect" },
+            { "name": "Sinkyone", "glottolog": "sink1245", "type": "dialect" },
+            { "name": "Wailaki", "glottolog": "wail1245", "type": "dialect" },
+          ]},
+          { "name": "Hupa-Chilula", "glottolog": "hupa1239", "type": "language", "cas": false, "children": [
+            { "name": "Chilula-Whilkut", "glottolog": "chil1285", "type": "dialect" },
+            { "name": "Hupa", "glottolog": "hupa1240", "type": "dialect" },
+          ]},
+          { "name": "Kato", "glottolog": "kato1244", "type": "language", "cas": false },
+          { "name": "Mattole-Bear River", "glottolog": "matt1238", "type": "language", "cas": false, "children": [
+            { "name": "Bear River", "glottolog": "bear1242", "type": "dialect" },
+            { "name": "Mattole", "glottolog": "matt1239", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Kwalhioqua-Clatskanie", "glottolog": "kwal1258", "type": "language", "cas": false, "children": [
+          { "name": "Clatskanie", "glottolog": "clat1234", "type": "dialect" },
+          { "name": "Kwalhioqua", "glottolog": "kwal1259", "type": "dialect", "children": [
+            { "name": "Suwal", "glottolog": "suwa1242", "type": "dialect" },
+            { "name": "Willapa", "glottolog": "will1244", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Oregon Athabaskan", "glottolog": "oreg1242", "type": "family", "children": [
+          { "name": "Rogue River", "glottolog": "rogu1236", "type": "family", "children": [
+            { "name": "Coquille", "glottolog": "coqu1236", "type": "language", "cas": false },
+            { "name": "Galice", "glottolog": "gali1261", "type": "language", "cas": false },
+            { "name": "Tututni", "glottolog": "tutu1242", "type": "language", "cas": false },
+          ]},
+          { "name": "Tolowa-Chetco", "glottolog": "tolo1259", "type": "language", "cas": false },
+          { "name": "Upper Umpqua", "glottolog": "uppe1436", "type": "language", "cas": false },
+        ]},
+      ]},
+      { "name": "Sarsi", "glottolog": "sars1236", "type": "language", "cas": false },
+      { "name": "Southern Alaskan Athabaskan", "glottolog": "sout3337", "type": "family", "children": [
+        { "name": "Ahtena", "glottolog": "ahte1237", "type": "language", "cas": false, "children": [
+          { "name": "Mentasta", "glottolog": "ment1252", "type": "dialect" },
+          { "name": "Nuclear Ahtna", "glottolog": "nucl1792", "type": "dialect", "children": [
+            { "name": "Central Copper River Ahtna", "glottolog": "cent2373", "type": "dialect" },
+            { "name": "Lower Copper River Ahtna", "glottolog": "lowe1450", "type": "dialect" },
+            { "name": "Western Ahtna", "glottolog": "west2953", "type": "dialect" },
+          ]},
+        ]},
+        { "name": "Dena’ina", "glottolog": "tana1289", "type": "language", "cas": false, "children": [
+          { "name": "Lower Inlet Denaina", "glottolog": "lowe1451", "type": "dialect" },
+          { "name": "Upper Inlet Denaina", "glottolog": "uppe1473", "type": "dialect" },
+        ]},
+      ]},
+      { "name": "Tsetsaut", "glottolog": "tset1236", "type": "language", "cas": false },
+    ]},
+    { "name": "Eyak", "glottolog": "eyak1241", "type": "language", "cas": false },
+  ]},
+  { "name": "Tlingit", "glottolog": "tlin1245", "type": "language", "cas": false, "children": [
+    { "name": "Northern Tlingit", "glottolog": "nort3355", "type": "dialect", "children": [
+      { "name": "Central Northern Tlingit", "glottolog": "cent2372", "type": "dialect" },
+      { "name": "Gulf Coast Northern Tlingit", "glottolog": "gulf1243", "type": "dialect" },
+      { "name": "Inland Northern Tlingit", "glottolog": "inla1272", "type": "dialect" },
+      { "name": "Transitional Northern Tlingit", "glottolog": "tran1298", "type": "dialect" },
+    ]},
+    { "name": "Southern Tlingit", "glottolog": "sout3338", "type": "dialect" },
+    { "name": "Tongass", "glottolog": "tong1333", "type": "dialect" },
+  ]},
+]}
+//[eof]
